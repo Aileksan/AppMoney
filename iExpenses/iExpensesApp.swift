20c13540fc58d8ef4b10a3227bf188737a88bcd4
myasik-S <62216082+myasik-S@users.noreply.github.com>
2023-08-24 03:00:57 +0700

    Initial Commit

---
+++ iExpenses/iExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensesApp.swift
+//  iExpenses
+//
+//  Created by Aleksandr Myasoedov on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
